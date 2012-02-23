81ec187e46c6a68563d56f1261e847fa2bcc2ddd
twoism <signalstatic@gmail.com>
2012-02-22 17:09:28 -0800

    using FinatraSpec for tests

--- src/test/scala/IntHandlerSpec.scala
+++ src/test/scala/IntHandlerSpec.scala
@@ -2,48 +2,14 @@ package com.capotej.structdb.tests
 import com.capotej.structdb.IntHandler
 
 import org.junit.Test
-import com.posterous.finatra.{FinatraServer, Router}
+import com.posterous.finatra.{FinatraServer, Router, FinatraSpec}
 import com.codahale.simplespec.Spec
 import com.twitter.finagle.http.{Http, RichHttp, Request, Response}
 import com.twitter.finagle.http.Status._
 import org.jboss.netty.handler.codec.http.{DefaultHttpRequest, DefaultHttpResponse, HttpMessage,
     HttpMethod, HttpRequest, HttpVersion, QueryStringEncoder}
 
-class IntegrationSpec extends Spec {
-
-  var lastResponse:Response = null
-
-  def get(uri: String, params: Tuple2[String, String]*) = {
-    val r = req(HttpMethod.GET, uri, params:_*)
-    lastResponse = Router.dispatch(r)
-  }
-
-  def post(uri: String, params: Tuple2[String, String]*) = {
-    val r = req(HttpMethod.POST, uri, params:_*)
-    lastResponse = Router.dispatch(r)
-  }
-
-  def post(uri: String) = {
-    val r       = Request(HttpMethod.POST, uri)
-    lastResponse  = Router.dispatch(r)
-  }
-
-  def get(uri: String) = {
-    val r       = Request(HttpMethod.GET, uri)
-    lastResponse  = Router.dispatch(r)
-  }
-  
-  def req(method: HttpMethod, uri: String, params: Tuple2[String, String]*): Request = {
-    val encoder = new QueryStringEncoder(uri)
-    params.foreach { case (key, value) =>
-      encoder.addParam(key, value)
-    }
-    Request(method, encoder.toString)
-  }
-
-}
-
-class IntHandlerSpec extends IntegrationSpec {
+class IntHandlerSpec extends FinatraSpec {
 
   FinatraServer.register(IntHandler)
 
